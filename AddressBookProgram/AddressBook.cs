d2a8715f145ac33df409d27eccffbd9d71f8cd17
kartiknawal <kartiknawal.7@gmail.com>
2020-10-27 11:07:06 +0530

    [kartik] Add. ability to add a new contact

--- AddressBookProgram/AddressBook.cs
+++ AddressBookProgram/AddressBook.cs
@@ -6,10 +6,16 @@ namespace AddressBookProgram
 {
     class AddressBook
     {
+
         List<Contacts> contactList;
         public AddressBook()
         {
             contactList = new List<Contacts>();
+        }
+        public void addContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string eMail)
+        {
+            Contacts contact = new Contacts(firstName, lastName, address, city, state, zipCode, phoneNo, eMail);
+            contactList.Add(contact);
         }
     }
 }
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -6,7 +6,18 @@ namespace AddressBookProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program!");
+            AddressBook addressBook = new AddressBook();
+            string[] details;
+
+            Console.WriteLine("Welcome to Address Book Program");
+
+            Console.WriteLine("Enter details separated by a comma");
+            Console.WriteLine("First Name, Last Name, Address, City, State, ZipCode, Email");
+            details = Console.ReadLine().Split(",");
+
+            addressBook.addContact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
+
+            Console.WriteLine("Details Added");
         }
     }
 }
