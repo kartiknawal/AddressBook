33caa4cca741378ae1f3cfda257b51f026bbaea4
kartiknawal <kartiknawal.7@gmail.com>
2020-10-27 11:00:55 +0530

    [kartik] Add. ability to create a contact

---
+++ AddressBookProgram/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProgram
+{
+    class AddressBook
+    {
+        List<Contacts> contactList;
+        public AddressBook()
+        {
+            contactList = new List<Contacts>();
+        }
+    }
+}
